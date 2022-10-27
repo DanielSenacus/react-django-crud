019a5cce2c4c304bfdb25a6d43ca94e9d2c231bf
Daniel S <danielsanchezvel@hotmail.com>
2022-10-27 01:47:01 -0500

    folder structure created

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar/Sidebar";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Empresas } from "./Empresas/Empresas";
+export { default as Login } from "./Login/Login";
+export { default as Users } from "./Users/Users";
